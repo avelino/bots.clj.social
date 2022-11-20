9e4c362a7ac1d963afa4a4554bfc4a0f41545265
Avelino <avelinorun@gmail.com>
2022-11-20 20:39:58 -0300

    process food clojure.org save after being published
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/social/clj/bots.cljs
+++ src/social/clj/bots.cljs
@@ -1,7 +1,9 @@
 (ns social.clj.bots
   (:require ["masto$login" :as login]
             ["redis$default" :as redis]
+            ["feed-reader$read" :as feed]
             [nbb.core :as nbb]
+            [clojure.walk :as walk]
             [promesa.core :as p]))
 
 (def masto
@@ -10,32 +12,54 @@
    (login #js{:url "https://clj.social"
               :accessToken js/process.env.ACCESS_TOKEN})))
 
-(defn toot [status visibility]
-  ;; public
+(def redis-conn
+  "redis client init"
+  (redis/createClient
+   #js{:url js/process.env.DATABASE_URL}))
+
+(defn toot [status visibility key]
+  "public toot"
   (nbb/await
    (p/let [body (.create masto.statuses #js{:status status
                                             :visibility visibility})
            obj (js->clj body)
            id (get obj "id")
            createdat (get obj "createdAt")]
-     {:createdat createdat :id id})))
+     {:toot-createdat createdat
+      :toot-id id
+      :key key})))
 
-(def redis-conn
-  "redis client init"
-  (redis/createClient
-   #js{:url js/process.env.DATABASE_URL}))
-
-(defn save [obj]
+(defn save [client obj]
   "save data in the storer"
-  (p/let [client redis-conn]
-    (.connect client)
-    (p/let [get (.get client (:id obj))]
-      (if-not get
-        (nbb/await (.set client (:id obj) (js/JSON.stringify (clj->js obj)))))
+  (let [key (:key obj)]
+    (prn :save key)
+    (nbb/await (.set client key (js/JSON.stringify (clj->js obj))))))
 
-      (.disconnect client)
+(defn toot-text [obj]
+  "formats the text that will be published"
+  (str (:title obj) "\n\n"
+       (:link obj) "\n\n"
+       (:description obj) "\n\n"
+       "#clojure #clj #cljs"))
 
-      (prn (:id obj)))))
+(defn feed-reader [client objs]
+  "do in all feed registration and publishing link (key)"
+  (let [itens (js->clj objs)
+        entries (sort-by :pubDate
+                         (walk/keywordize-keys (get itens "entries")))]
+    (doseq [obj entries]
+      (p/let [get (.get client (:link obj))]
+        (if-not get
+          (let [body (toot-text obj)]
+            ;; (nbb/await (.set client (:key x) (js/JSON.stringify (clj->js x))))
+            ;; public
+            (.then (toot body "private" (:link obj))
+                   (fn [x] (save client x)))))))))
 
 (defn -main []
-  (.then (toot "hello" "private") save))
+  (p/let [client redis-conn]
+    (.connect client)
+    (.then (feed "https://clojure.org/feed.xml")
+           (fn [x] (feed-reader client x)))
+    ;; (.disconnect client)
+    ))
